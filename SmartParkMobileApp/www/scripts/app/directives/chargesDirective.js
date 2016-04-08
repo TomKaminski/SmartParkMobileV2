c6f99537f67cfe0e60f3a888b272bb289c98d0f8
Tomasz Kamiński <tkaminski93@gmail.com>
2016-04-08 22:59:51 +0200

    fix for edge charges bug ......

---
+++ SmartParkMobileApp/www/scripts/app/directives/chargesDirective.js
@@ -0,0 +1,18 @@
+﻿angular.module('app').directive('charges', ['$timeout', 'accountService', function ($timeout, accountService) {
+
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: {
+            loadname: '@'
+        },
+        template: '<span></span>',
+        link: function ($scope, element) {
+            $scope.$watch(function () {
+                return accountService.userData.charges;
+            }, function () {
+                element.text(accountService.userData.charges);
+            });
+        }
+    };
+}]);
